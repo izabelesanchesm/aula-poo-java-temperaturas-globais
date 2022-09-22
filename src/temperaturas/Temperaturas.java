51685f3f5f54904243817b25956f429b192a9219
isanchesm <izabele.sanchesm@mercadolivre.com>
2022-09-22 18:46:55 -0300

    ajustando escrita dos vetores

--- src/temperaturas/Temperaturas.java
+++ src/temperaturas/Temperaturas.java
@@ -3,43 +3,10 @@ package temperaturas;
 public class Temperaturas {
 
     public static void main(String[] args) {
-        String cidades[] = new String[10];
-        int temperaturas[][] = new int[10][2];
+        String cidades[] = {"Londres", "Madrid", "Nova York", "Buenos Aires", "Assunción", "São Paulo", "Lima", "Santiago de Chile", "Lisboa", "Tokio"};
+        int[][] temperaturas = {{-2, 33}, {-3, 32}, {-8, 27}, {4, 37}, {6, 42}, {5, 43}, {0, 39}, {-7, 26}, {-1, 31}, {-10, 35}};
         int cidadeMenorTemperatura = 0;
         int cidadeMaiorTemperatura = 0;
-
-        cidades[0] = "Londres";
-        cidades[1] = "Madrid";
-        cidades[2] = "Nueva York";
-        cidades[3] = "Buenos Aires";
-        cidades[4] = "Asunción";
-        cidades[5] = "São Paulo";
-        cidades[6] = "Lima";
-        cidades[7] = "Santiago de Chile";
-        cidades[8] = "Lisboa";
-        cidades[9] = "Tokio";
-
-        temperaturas[0][0] = -2;
-        temperaturas[0][1] = 33;
-        temperaturas[1][0] = -3;
-        temperaturas[1][1] = 32;
-        temperaturas[2][0] = -8;
-        temperaturas[2][1] = 27;
-        temperaturas[3][0] = 4;
-        temperaturas[3][1] = 37;
-        temperaturas[4][0] = 6;
-        temperaturas[4][1] = 42;
-        temperaturas[5][0] = 5;
-        temperaturas[5][1] = 43;
-        temperaturas[6][0] = 0;
-        temperaturas[6][1] = 39;
-        temperaturas[7][0] = -7;
-        temperaturas[7][1] = 26;
-        temperaturas[8][0] = -1;
-        temperaturas[8][1] = 31;
-        temperaturas[9][0] = -10;
-        temperaturas[9][1] = 35;
-
         int menorTemperatura = temperaturas[0][0];
         int maiorTemperatura = temperaturas[0][1];
 
